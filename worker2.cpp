8c218433c0049ffd732f58b07ce7b284fbe8df6f
hai dong <hai_dong1@yahoo.com>
2014-12-14 16:24:07 +0100

    Update worker2.cpp
    a simple background worker example shows Progress bar

--- worker2.cpp
+++ worker2.cpp
@@ -18,6 +18,8 @@ namespace worker2 {
 	public ref class Form1 : public System::Windows::Forms::Form
 	{
 		int highestPercentageReached;
+	private: System::Windows::Forms::ProgressBar^  progressBar1;
+			 BackgroundWorker^ worker;
 	public:
 		Form1(void)
 		{
@@ -44,11 +46,6 @@ namespace worker2 {
 
 
 	private: System::ComponentModel::BackgroundWorker^  backgroundWorker1;
-
-
-
-
-
 	private:
 		/// <summary>
 		/// Required designer variable.
@@ -64,6 +61,7 @@ namespace worker2 {
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->backgroundWorker1 = (gcnew System::ComponentModel::BackgroundWorker());
+			this->progressBar1 = (gcnew System::Windows::Forms::ProgressBar());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -76,11 +74,20 @@ namespace worker2 {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
+			// progressBar1
+			// 
+			this->progressBar1->Location = System::Drawing::Point(33, 94);
+			this->progressBar1->Name = L"progressBar1";
+			this->progressBar1->Size = System::Drawing::Size(203, 23);
+			this->progressBar1->Step = 1;
+			this->progressBar1->TabIndex = 1;
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(260, 141);
+			this->Controls->Add(this->progressBar1);
 			this->Controls->Add(this->button1);
 			this->Name = L"Form1";
 			this->Text = L"worker test";
@@ -92,18 +99,21 @@ namespace worker2 {
 	private: 
 		void backgroundWorker1_DoWork( Object^ sender, DoWorkEventArgs^ e )
 		{
-			BackgroundWorker^ worker = dynamic_cast<BackgroundWorker^>(sender);
+			worker = dynamic_cast<BackgroundWorker^>(sender);
+			worker->WorkerReportsProgress = true;
+
 			e->Result = doSomeWork(worker, e );
 		}
 
 		void backgroundWorker1_RunWorkerCompleted( Object^ /*sender*/, RunWorkerCompletedEventArgs^ e )
 		{
-			 MessageBox::Show( "Complete" );
+			MessageBox::Show( "Complete" );
 		}
 
 		// This event handler updates the progress bar. 
 		void backgroundWorker1_ProgressChanged( Object^ /*sender*/, ProgressChangedEventArgs^ e )
 		{
+			this->progressBar1->Value = e->ProgressPercentage;
 		}
 
 		void InitializeBackgoundWorker()
@@ -117,10 +127,13 @@ namespace worker2 {
 		long doSomeWork( BackgroundWorker^ worker, DoWorkEventArgs ^ e )
 		{		
 			Bitmap^ bitmap1  = dynamic_cast<Bitmap^>(Bitmap::FromFile("D:\\image1.jpg"));
-			for(int ii=0; ii<10;ii++){				
+			for(int ii=1; ii<100;ii++){				
 				bitmap1->RotateFlip( RotateFlipType::Rotate90FlipY );
-				bitmap1->Save("D:\\image22" + ii + ".jpg");		
+				bitmap1->Save("D:\\image22" + ii + ".jpg");	
+				if(ii%11 == 0)
+					worker->ReportProgress( ii );
 			}
+			worker->ReportProgress( 100);
 			return 0;
 		}
 
@@ -130,7 +143,7 @@ namespace worker2 {
 			 }
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
 				 this->button1->Enabled = false;
-				 backgroundWorker1->RunWorkerAsync( );
+				 backgroundWorker1->RunWorkerAsync( );				 
 			 }
 	};
 }
